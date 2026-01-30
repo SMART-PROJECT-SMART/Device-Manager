a391e671b4bc3e7f8eff0b1920e9f87de58191d5
jonathan kalush <jonathan.kalush@gmail.com>
2026-01-30 15:13:16 +0200

    [feat] base sleeve mocels

---
+++ DeviceManager/Database/MongoDB/Entities/Sleeve.cs
@@ -0,0 +1,23 @@
+using Core.Models;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DeviceManager.Database.MongoDB.Entities
+{
+    public class Sleeve
+    {
+        public Sleeve(string name, Location location, IEnumerable<int> portNumbers)
+        {
+            Name = name;
+            Location = location;
+            PortNumbers = portNumbers;
+        }
+
+        [BsonRequired]
+        [BsonId]
+        public string Name { get; set; }
+        [BsonRequired]
+        public Location Location { get; set; }
+        [BsonRequired]
+        public IEnumerable<int> PortNumbers { get; set; }
+    }
+}
